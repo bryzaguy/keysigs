3d8be40b12b14570999bb72ea94153b488086a0e
Bryant Syme <prefu@hotmail.com>
2021-04-26 19:42:19 -0600

    working version of game

--- src/App.js
+++ src/App.js
@@ -1,37 +1,88 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
-const pickRandom = arr => arr[Math.ceil(Math.random() * arr.length) - 1]
+const pickRandom = (arr) => arr[Math.ceil(Math.random() * arr.length) - 1];
+
+const prizeLink =
+  "https://docs.google.com/document/d/1e8bCDXDnz1Y-qnNY2vTknYMVThinHRO2Ur9eF1V1DZk/edit?usp=sharing";
 
 const major_letters = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G',
-  'C#', 'F#', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Gb'
-].sort()
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "C#",
+  "F#",
+  "Ab",
+  "Bb",
+  "Cb",
+  "Db",
+  "Eb",
+  "Gb",
+].sort();
 
 const minor_letters = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G',
-  'A#', 'C#', 'D#', 'G#',
-  'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Gb'
-].sort()
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "A#",
+  "C#",
+  "D#",
+  "G#",
+  "Ab",
+  "Bb",
+  "Cb",
+  "Db",
+  "Eb",
+  "Gb",
+].sort();
 
-const letter = pickRandom(major_letters)
+const types = ["maj", "min"];
+const clefs = ["treble", "bass"];
 
-const className = letter.replace('#', 's')
-                        .replace('b', 'f')
-                        .toLowerCase() + '-maj'
+const newGame = (lastResult) => {
+  const type = pickRandom(types);
+  const letters = type === "maj" ? major_letters : minor_letters;
+  const letter = pickRandom(letters);
+  const clef = pickRandom(clefs);
+
+  return { clef, type, letter, letters, lastResult };
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [{ clef, type, letter, letters, lastResult }, setState] = useState(
+    newGame()
+  );
+
+  const cssLetter = letter.replace("#", "s").replace("b", "f").toLowerCase();
+  const className = `${clef}-${cssLetter}-${type}`;
+
+  const onClick = (e) => {
+    const game = newGame(e.target.textContent === letter)
+    setState(game)
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div className={`App-logo treble-${className}`} />
+        <div className='Result' style={{color: lastResult ? 'green' : 'red'}}>{lastResult ? 'GOT IT!' : 'BUMMER'}</div>
+        <div className={`App-logo ${className}`} />
         <div className="Answers">
-          {major_letters.map(
-            key => <button>{key}</button>
-          )}
+          {letters.map((key) => (
+            <button key={key} onClick={onClick}>{key}</button>
+          ))}
         </div>
       </header>
+      <a href={prizeLink} style={{ display: "hidden" }}>
+        {prizeLink}
+      </a>
     </div>
   );
 }
