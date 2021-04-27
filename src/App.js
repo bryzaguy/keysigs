f5398985c1ca88f3662666a1b779f1b862342648
Bryant Syme <prefu@hotmail.com>
2021-04-26 18:35:23 -0600

    add stuffs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const pickRandom = arr => arr[Math.ceil(Math.random() * arr.length) - 1]
+
+const major_letters = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G',
+  'C#', 'F#', 'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Gb'
+].sort()
+
+const minor_letters = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G',
+  'A#', 'C#', 'D#', 'G#',
+  'Ab', 'Bb', 'Cb', 'Db', 'Eb', 'Gb'
+].sort()
+
+const letter = pickRandom(major_letters)
+
+const className = letter.replace('#', 's')
+                        .replace('b', 'f')
+                        .toLowerCase() + '-maj'
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className={`App-logo treble-${className}`} />
+        <div className="Answers">
+          {major_letters.map(
+            key => <button>{key}</button>
+          )}
+        </div>
       </header>
     </div>
   );
